4b16b23ce17bd60d1a37c6d0efec30cceaaac9a7
Thomas Zilio <Dasc3er@users.noreply.github.com>
2020-03-17 18:28:24 +0100

    Aggiunta gestione causale

---
+++ src/Fields/Text.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Dasc3er\FatturaElettronica\Fields;
+
+use ArrayIterator;
+use Dasc3er\FatturaElettronica\Interfaces\FieldInterface;
+use Dasc3er\FatturaElettronica\Interfaces\UnserializeInterface;
+use IteratorAggregate;
+
+/**
+ * Classe per la gestione virtuale di elementi che possono essere prsenti più volte in una stessa sezione.
+ */
+class Collection implements IteratorAggregate, FieldInterface, UnserializeInterface
+{
+    protected $values;
+    protected string $class;
+
+    public function __construct(string $class)
+    {
+        $this->class = $class;
+
+        $this->values = [];
+    }
+
+    /**
+     * Aggiunge un elemento alla collezione.
+     *
+     * @param $value
+     */
+    public function add($value): ?int
+    {
+        if ($value instanceof $this->class) {
+            $this->values[] = $value;
+
+            return array_key_last($this->values);
+        }
+
+        return null;
+    }
+
+    /**
+     * Rimuove l'elemento di indice indicato dalla collezione.
+     */
+    public function remove(int $index): void
+    {
+        if (isset($this->values[$index])) {
+            unset($this->values[$index]);
+        }
+    }
+
+    /**
+     * Restituisce l'elemento di indice indicato dalla collezione.
+     */
+    public function getElement(int $index)
+    {
+        if (isset($this->values[$index])) {
+            return $this->values[$index];
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($value): void
+    {
+        // TODO: Implement set() method.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get()
+    {
+        return $this->toArray();
+    }
+
+    public function toArray(): array
+    {
+        return $this->values;
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->values);
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->values);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize(array $content): void
+    {
+        if (!isset($content[0])) {
+            $content = [$content];
+        }
+
+        $class = $this->class;
+        foreach ($content as $i => $var) {
+            $element = new $class();
+            $element->unserialize($var);
+
+            $this->add($element);
+        }
+    }
+}
